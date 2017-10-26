448b224b5f91dcb5edc6fd3ede0cdefbf8eda4b7
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-10-26 10:29:37 -0200

    Update main.c

--- main.c
+++ main.c
@@ -16,10 +16,29 @@ typedef struct Livro
 Livro *primeiro, *atual, *NovoLivro;
 
 void GetLivro();
+void PrintLivro();
 
 main(){
 
+
+    char ch;
+    primeiro = (Livro*) NULL;
+
+    do{
+
 GetLivro();
+puts("\n Inserir outrolivro (s/n): ");
+ch = getch();
+
+    }while((ch != 'n') && (ch != 'N'));
+
+    puts("\n Lista dos livros cadastrados");
+    puts("===============================");
+
+PrintLivro();
+
+return 0;
+
 
 }
 
@@ -30,7 +49,7 @@ void GetLivro(){
     char temp[80];
     NovoLivro = (Livro*) malloc(sizeof(Livro));
 
-    if(primeiro == (Livro*)NULL){primeiro = atual=NovoLivro;}
+    if(primeiro == (Livro*)NULL){primeiro = atual = NovoLivro;}
         else{
             atual = primeiro;
             while(atual->Proximo != (Livro*) NULL){
@@ -54,8 +73,32 @@ void GetLivro(){
         atual->Preco = atof(temp);
         atual->Proximo = (Livro*) NULL;
 
+}
+
+void PrintLivro(){
+
+if (primeiro == (Livro*) NULL){
+    puts("Lista vazia");
+    return;
+}
+
+atual = primeiro;
+do{
 
 
+    printf("Titulo: %s\n",atual->Titulo);
+    printf("Autor: %s\n",atual->Autor);
+    printf("No. Reg:  %d\n",atual->NumReg);
+    printf("Preco:  %.2f\n\n",atual->Preco);
+
+if(atual->Proximo == (Livro*)NULL){
+break;
+}else{
+atual = atual->Proximo;
+}
+
+
+}while(atual->Proximo);
 }
 
 
